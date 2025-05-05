4b8ac9556e9fa598a4258eb161e38294099c73ba
Olha <olha.kravchenko@oa.edu.ua>
2025-05-06 00:20:47 +0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/lab5-ci-cd/', // назва вашого репозиторію
+  build: {
+    outDir: 'dist', // <- ця частина обов’язкова для GitHub Pages
+  },
 });
